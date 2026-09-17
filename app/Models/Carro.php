439103651ed1ce443dcc444599beea81c9867b14
vinicius <engs-viniciusgardacho@camporeal.edu.br>
2026-09-16 21:47:13 -0300

    Models

--- app/Models/Carro.php
+++ app/Models/Carro.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Carro extends Model
 {
-    //
+    protected $fillable = ['marca_id', 'modelo', 'ano', 'preco'];
+
+    protected function casts(): array
+    {
+        return ['ano' => 'integer', 'preco' => 'decimal:2'];
+    }
+
+    public function marca()
+    {
+        return $this->belongsTo(Marca::class);
+    }
 }
--- app/Models/Marca.php
+++ app/Models/Marca.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Marca extends Model
 {
-    //
+    protected $fillable = ['nome'];
+
+    public function carros()
+    {
+        return $this->hasMany(Carro::class);
+    }
 }
