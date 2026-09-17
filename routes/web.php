51ebd03416ff007f2bfdda3c747285e609e845a6
vinicius <engs-viniciusgardacho@camporeal.edu.br>
2026-09-16 21:51:11 -0300

    routs

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\CarroController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', DashboardController::class)->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::resource('carros', CarroController::class)->only(['index']);
+    Route::resource('carros', CarroController::class)->only(['create', 'store', 'edit', 'update', 'destroy'])->middleware('admin');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
